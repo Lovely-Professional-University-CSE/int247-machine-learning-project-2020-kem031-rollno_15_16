bc30b5c39ba5c3f5f1836ab9d613fd7feb4970a7
Kiran Pathro <47917842+KiranPathro@users.noreply.github.com>
2020-04-03 21:03:04 +0530

    plot_confusion_matrix

--- fake_news_detection(linear_model)/fake_news_detection1.py
+++ fake_news_detection(linear_model)/fake_news_detection1.py
@@ -35,10 +35,49 @@ tfidf_test=tfidf_vectorizer.transform(x_test)
 #Initialize a PassiveAggressiveClassifier
 pac=PassiveAggressiveClassifier(max_iter=50)
 pac.fit(tfidf_train,y_train)
+
+
+#DataFlair - Build confusion matrix
+confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
+
+
+# Plotting the confusion matrix using matplotlib
+#%matplotlib inline
+import sklearn.metrics as metrics
+confusion = metrics.confusion_matrix(y_test,y_pred)
+# Plot non-normalized confusion matrix
+import matplotlib.pyplot as plt
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.RdYlGn):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+plt.figure()
+plot_confusion_matrix(confusion, classes=['FAKE','REAL'],
+ title='Confusion matrix, without normalization')
+
 #Predict on the test set and calculate accuracy
 y_pred=pac.predict(tfidf_test)
 score=accuracy_score(y_test,y_pred)
 print(f'Accuracy: {round(score*100,2)}%')
-
-#Build confusion matrix
-print(confusion_matrix(y_test,y_pred, labels=['FAKE','REAL']))
